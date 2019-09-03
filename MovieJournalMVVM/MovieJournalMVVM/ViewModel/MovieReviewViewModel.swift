27ef0eec78c18b6435f9c13f4f0d99ba3068aba4
Lauren Small <laurensmall@laurens-mbp-2.attlocal.net>
2019-09-02 20:51:51 -0400

    reintroduce the MovieReviewViewModel

---
+++ MovieJournalMVVM/MovieJournalMVVM/ViewModel/MovieReviewViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  MovieReviewViewModel.swift
+//  MovieJournalMVVM
+//
+//  Created by Lauren Small on 9/2/19.
+//  Copyright © 2019 Lauren Small. All rights reserved.
+//
+
+import Foundation
