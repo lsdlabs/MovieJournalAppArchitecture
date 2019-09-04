0238370058eef20c760ed62e7512f1b037d9bffc
Lauren Small <laurensmall@laurens-mbp-2.attlocal.net>
2019-09-04 14:51:12 -0400

    modify view model and add a MovieReviewListViewModel, need to figure out and decide how to structure view model(s)

--- MovieJournalMVVM/MovieJournalMVVM/ViewModel/MovieReviewViewModel.swift
+++ MovieJournalMVVM/MovieJournalMVVM/ViewModel/MovieReviewViewModel.swift
@@ -8,14 +8,55 @@
 
 import Foundation
 
-struct MovieReviewViewModel {
-    var title: String
-    var review: String
-    var movieReview: MovieReview
+//struct MovieReviewViewModel {
+//    var title: String
+//    var review: String
+//    var movieReview: MovieReview
+//
+//    init(movieReview: MovieReview){
+//        self.title = movieReview.title
+//        self.review = movieReview.review
+//        self.movieReview = movieReview
+//    }
+//}
+
+struct MovieReviewListViewModel {
+    let movieReviews: [MovieReview]
+}
+
+extension MovieReviewListViewModel {
+    var numberOfSections: Int {
+        return 1
+    }
     
-    init(movieReview: MovieReview){
-        self.title = movieReview.title
-        self.review = movieReview.review
+    func numberOfRowsInSection(_ section: Int) -> Int {
+        return self.movieReviews.count
+    }
+    
+    func movieReviewAtIndex(_ index: Int) -> MovieReviewViewModel {
+        let movieReview = self.movieReviews[index]
+        return MovieReviewViewModel(movieReview)
+    }
+}
+
+
+struct MovieReviewViewModel {
+    private let movieReview: MovieReview
+}
+
+
+extension MovieReviewViewModel {
+    init(_ movieReview: MovieReview){
         self.movieReview = movieReview
+    }
+}
+
+//What about UUID?  It isn't part of the view...
+extension MovieReviewViewModel {
+    var title: String {
+        return self.movieReview.title
+    }
+    var review: String {
+        return self.movieReview.review
     }
 }
